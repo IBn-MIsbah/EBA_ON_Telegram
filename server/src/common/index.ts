5c439d03e9ad703de443b267e284ea7bcadb84bf
IBn-MIsbah <160459932+IBn-MIsbah@users.noreply.github.com>
2025-12-21 23:51:54 +0300

    Commonly use constansts

---
+++ server/src/common/index.ts
@@ -0,0 +1,5 @@
+export const isProduction = process.env.NODE_ENV === "production";
+export const BOT_TOKEN = process.env.BOT_TOKEN as string;
+export const TG_WEBHOOK_SECRET =
+  (process.env.TG_WEBHOOK_SECRET as string) || "okmnjiuhb";
+export const WEBHOOK_URL = process.env.WEBHOOK_URL as string;
