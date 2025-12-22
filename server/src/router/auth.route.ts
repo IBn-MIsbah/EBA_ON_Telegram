db7922d3a0411b8b263d00419c5823ca9080b02a
IBn-MIsbah <160459932+IBn-MIsbah@users.noreply.github.com>
2025-12-22 22:36:44 +0300

    feat: Auth api routes

---
+++ server/src/router/auth.route.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import { AuthController } from "../controller/auth.controller.js";
+import { authenticateUser } from "../middleware/auth.middleware.js";
+
+const router = Router();
+
+router
+  .post("/login", AuthController.login)
+  .post("/logout", AuthController.logout)
+  .get("/me", authenticateUser, AuthController.me)
+  .post("/refresh-token", authenticateUser, AuthController.refreshToken);
+//.post('/update') // update credential
+
+export default router;
