f2a56073f0df485adfc2c34ae9ce8e0d66eaa14e
IBn-MIsbah <160459932+IBn-MIsbah@users.noreply.github.com>
2025-12-23 21:02:35 +0300

    feat: Added auth middleware

--- server/src/router/user.route.ts
+++ server/src/router/user.route.ts
@@ -1,7 +1,10 @@
 import { Router } from "express";
 import { UserController } from "../controller/user.controller.js";
+import { authenticateUser } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
-router.get("/", UserController.getAll).post("/", UserController.create);
+router
+  .get("/", authenticateUser, UserController.getAll)
+  .post("/", authenticateUser, UserController.create);
 export default router;
