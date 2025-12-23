d9a8a4f41c037785c71c02f4d869df458f0a9246
IBn-MIsbah <160459932+IBn-MIsbah@users.noreply.github.com>
2025-12-23 23:40:13 +0300

    Added gender and additional roles

--- server/src/schema/userSchema.ts
+++ server/src/schema/userSchema.ts
@@ -5,13 +5,19 @@ export const userSchema = z.object({
   name: z.string(),
   phone: z.string(),
   telegramUserId: z.string().optional(),
-  role: z.enum(["USER", "ADMIN"]).default("USER"),
+  role: z
+    .enum(["USER", "AMIR", "AMIRA", "VICEAMIR", "VICEAMIRA", "ADMIN"])
+    .default("USER"),
+  gender: z.enum(["MALE", "FEMALE"]),
 });
 
 export const userCreateInputSchema = z.object({
   name: z.string(),
   phone: z.string().max(10).min(10),
-  role: z.enum(["USER", "ADMIN"]).default("USER"),
+  role: z
+    .enum(["USER", "AMIR", "AMIRA", "VICEAMIR", "VICEAMIRA", "ADMIN"])
+    .default("USER"),
+  gender: z.enum(["MALE", "FEMALE"]),
   password: z.string().min(6),
   telegramUserId: z.string().optional(),
 });
