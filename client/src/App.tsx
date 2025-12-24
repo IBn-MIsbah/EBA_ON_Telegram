3fea40d3a6f3f434438fb0487bf160ea2db03a49
IBn-MIsbah <160459932+IBn-MIsbah@users.noreply.github.com>
2025-12-24 22:29:53 +0300

    Login flow demo

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,28 @@
 import "./App.css";
+import LoginForm from "./components/form/LoginForm";
+import type { LoginInput } from "./schemas/auth-schema";
+import { postLogin } from "./services/auth-api";
 
 function App() {
-  return <h1 className="font-extrabold text-2xl">EBA on Telegram</h1>;
+  const handleLoginSubmit = async (data: LoginInput) => {
+    try {
+      console.log(data);
+      const response = await postLogin(data);
+      console.log(response);
+      alert("Login success");
+    } catch (error) {
+      console.log(error);
+      alert("Login failed");
+    }
+  };
+  return (
+    <>
+      {/* <h1 className="font-extrabold text-2xl">EBA on Telegram</h1> */}
+      <div className="flex h-screen w-screen justify-center items-center bg-stone-300">
+        <LoginForm onSubmit={handleLoginSubmit} />
+      </div>
+    </>
+  );
 }
 
 export default App;
