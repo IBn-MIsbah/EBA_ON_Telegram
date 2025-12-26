4953aad0ef8a010b3a41b58138c5dde0dc1699ba
IBn-MIsbah <160459932+IBn-MIsbah@users.noreply.github.com>
2025-12-26 23:05:51 +0300

    feat: protected route to handle auth

---
+++ client/src/components/ProtectedRoute.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { useAuth } from "../context/auth-context";
+import { Navigate, useLocation } from "react-router-dom";
+
+interface ProtectedRouteProps {
+  children: React.ReactNode;
+}
+export const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
+  const { isAuthenticated, isLoading } = useAuth();
+  const location = useLocation();
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!isAuthenticated) {
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+
+  return <>{children}</>;
+};
