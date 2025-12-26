1a7be39dd5f5d9bb1185d368899599f4739905e4
IBn-MIsbah <160459932+IBn-MIsbah@users.noreply.github.com>
2025-12-26 10:53:10 +0300

    redirect with auth check

--- client/src/page/Login.tsx
+++ client/src/page/Login.tsx
@@ -2,14 +2,18 @@ import React from "react";
 import LoginForm from "../components/form/LoginForm";
 import type { LoginInput } from "../schemas/auth-schema";
 import { postLogin } from "../services/auth-api";
+import { useNavigate } from "react-router-dom";
 
 const Login: React.FC = () => {
+  const navigate = useNavigate();
   const handleLoginSubmit = async (data: LoginInput) => {
     try {
       console.log(data);
       const response = await postLogin(data);
+
       console.log(response);
       alert("Login success");
+      navigate("/");
     } catch (error) {
       console.log(error);
       alert("Login failed");
