84a790263bb6669e518e39b1a4d3ce55a9c82792
IBn-MIsbah <160459932+IBn-MIsbah@users.noreply.github.com>
2025-12-26 10:03:35 +0300

    router config demo

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,32 +1,9 @@
+import { RouterProvider } from "react-router-dom";
 import "./App.css";
-// import ImageUpload from "./components/form/CreateProductForm";
-import ListProducts from "./components/form/ListProducts";
-// import LoginForm from "./components/form/LoginForm";
-// import type { LoginInput } from "./schemas/auth-schema";
-// import { postLogin } from "./services/auth-api";
+import { router } from "./router";
 
 function App() {
-  // const handleLoginSubmit = async (data: LoginInput) => {
-  // try {
-  // console.log(data);
-  // const response = await postLogin(data);
-  // console.log(response);
-  // alert("Login success");
-  // } catch (error) {
-  // console.log(error);
-  // alert("Login failed");
-  // }
-  // };
-  return (
-    <>
-      {/* <h1 className="font-extrabold text-2xl">EBA on Telegram</h1> */}
-      <div className="flex h-screen w-screen justify-center items-center bg-stone-300">
-        {/* <LoginForm onSubmit={handleLoginSubmit} /> */}
-        {/* <ImageUpload /> */}
-        <ListProducts />
-      </div>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
