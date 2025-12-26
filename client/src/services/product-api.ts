0f2d29d587b8c566d335dee0fe8a43cb1cce5b25
IBn-MIsbah <160459932+IBn-MIsbah@users.noreply.github.com>
2025-12-26 22:24:33 +0300

    feat: Added product update api function

--- client/src/services/product-api.ts
+++ client/src/services/product-api.ts
@@ -17,3 +17,10 @@ export const getProductById = async (id: string) => {
   const response = await api.get(`/product/${id}`);
   return response.data;
 };
+
+export const updatedProduct = async (id: string, data: FormData) => {
+  const response = await api.put(`/product/${id}`, data, {
+    headers: { "Content-Type": "multipart/form-data" },
+  });
+  return response.data;
+};
