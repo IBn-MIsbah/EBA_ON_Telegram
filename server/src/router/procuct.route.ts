17881091c33e8311eacc8c085764dc4991c48fcc
IBn-MIsbah <160459932+IBn-MIsbah@users.noreply.github.com>
2025-12-27 09:40:25 +0300

    feat: Added auth middleware

--- server/src/router/procuct.route.ts
+++ server/src/router/procuct.route.ts
@@ -1,14 +1,20 @@
 import { Router } from "express";
 import { ProductController } from "../controller/product.controller.js";
 import { upload } from "../middleware/upload.js";
+import { authenticateUser } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
 router
-  .post("/", upload.single("image"), ProductController.create)
-  .get("/", ProductController.getProduct)
-  .get("/:id", ProductController.getProductById)
-  .patch("/:id", upload.single("image"), ProductController.updateProduct)
-  .delete("/:id", ProductController.deleteProduct);
+  .post("/", authenticateUser, upload.single("image"), ProductController.create)
+  .get("/", authenticateUser, ProductController.getProduct)
+  .get("/:id", authenticateUser, ProductController.getProductById)
+  .patch(
+    "/:id",
+    authenticateUser,
+    upload.single("image"),
+    ProductController.updateProduct
+  )
+  .delete("/:id", authenticateUser, ProductController.deleteProduct);
 
 export default router;
