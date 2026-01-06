585fd2289a048f94ff02ed8e217f9bfe9b8bf021
ibn-misbah <kawnuma@gmail.com>
2026-01-06 22:33:18 +0300

    feat: Added salt round constant

--- server/src/common/index.ts
+++ server/src/common/index.ts
@@ -3,3 +3,4 @@ export const BOT_TOKEN = process.env.BOT_TOKEN as string;
 export const TG_WEBHOOK_SECRET =
   (process.env.TG_WEBHOOK_SECRET as string) || "okmnjiuhb";
 export const WEBHOOK_URL = process.env.WEBHOOK_URL as string;
+export const SALT_ROUND = process.env.SALT_ROUND as string;
