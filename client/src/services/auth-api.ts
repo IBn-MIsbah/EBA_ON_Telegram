0d078d37e136ed2d6e3d30fd78686c1c1c05b55c
ibn-misbah <kawnuma@gmail.com>
2026-01-06 22:43:58 +0300

    feat: PATCH request to update user data

--- client/src/services/auth-api.ts
+++ client/src/services/auth-api.ts
@@ -2,24 +2,30 @@ import {
   type LoginInput,
   type Login,
   type Me,
+  type UpdateUserBody,
 } from "../schemas/auth-schema";
 import { api } from "./api-client";
 
 export const postLogin = async (data: LoginInput): Promise<Login> => {
-    const response = await api.post("/auth/login", data);
-    return response.data;
+  const response = await api.post("/auth/login", data);
+  return response.data;
 };
 
 export const postLogout = async () => {
-    const response = await api.post("/auth/logout");
-    return response.data;
+  const response = await api.post("/auth/logout");
+  return response.data;
 };
 
 export const getMe = async (): Promise<Me> => {
-    const response = await api.get("/auth/me");
-    return response.data;
-  }
+  const response = await api.get("/auth/me");
+  return response.data;
+};
 
 export const postRefresh = async () => {
   await api.post("/auth/refresh-token");
+};
+
+export const patchData = async (id: string, data: UpdateUserBody) => {
+  const response = await api.patch(`/auth/update/${id}`, data);
+  return response.data;
 };
