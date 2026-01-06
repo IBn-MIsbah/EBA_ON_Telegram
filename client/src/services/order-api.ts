ca4a660065c1a1f66cd99546eb0c77a3e9e9a69a
ibn-misbah <kawnuma@gmail.com>
2026-01-06 22:40:54 +0300

    feat: add verifyOrder function to handle order verification

--- client/src/services/order-api.ts
+++ client/src/services/order-api.ts
@@ -5,3 +5,8 @@ export const getOrders = async (): Promise<OrderResponse> => {
   const response = await api.get("/orders");
   return response.data;
 };
+
+export const verifyOrder = async (orderId: string) => {
+  const response = await api.patch(`/orders/verify/${orderId}`);
+  return response.data;
+};
