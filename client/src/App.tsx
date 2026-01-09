1e698cc2c5de483c4fec707248164d8d70f97989
ibn-misbah <kawnuma@gmail.com>
2026-01-09 23:00:07 +0300

    feat: added toaster provider

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,10 +2,12 @@ import { RouterProvider } from "react-router-dom";
 import "./App.css";
 import { router } from "./router";
 import { AuthProvider } from "./context/auth-context";
+import { Toaster } from "sonner";
 
 function App() {
   return (
     <AuthProvider>
+      <Toaster position="top-center" richColors />
       <RouterProvider router={router} />
     </AuthProvider>
   );
